dc446fcb23d516d0ef9ec9bc75c1e9277ff5f466
Shawn Bittinger <shawn.bittinger@appian.com>
2022-12-03 18:09:57 +0000

    New Menu Class

---
+++ src/main/java/bank/Menu.java
@@ -0,0 +1,44 @@
+package bank;
+
+import java.util.Scanner;
+
+import javax.security.auth.login.LoginException;
+
+public class Menu {
+  private Scanner scanner;
+
+  public static void main(String[] args){
+    System.out.println("Welcome to Shawn's Bank");
+    Menu menu = new Menu();
+    menu.scanner = new Scanner(System.in);
+
+    Customer customer = menu.authenticateUser();
+    if(customer != null){
+      Account account = DataSource.getAccount(customer.getAccountId());
+      menu.showMenu(customer, account);
+    }
+
+
+    menu.scanner.close();
+  }
+
+  private Customer authenticateUser(){
+    System.out.println("Enter your username");
+    String username = scanner.next();
+    System.out.println("Enter your password");
+    String password = scanner.next();
+    Customer customer = null;
+    try{
+      customer = Authenticator.login(username, password);
+    } catch(LoginException e){
+      System.out.println(e);
+    }
+
+    return customer;
+  }
+
+  private void showMenu(Customer customer, Account account) {
+    
+  }
+  
+}
